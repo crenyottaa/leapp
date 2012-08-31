eda1026e22a91a36d39ec74c5d16c0b1dae264e4
root <root@LEUT12.(none)>
2012-08-31 16:14:53 +0800

    add files

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "leaapplication"
+set :repository,  "git@github.com:crenyottaa/leapp.git"
+set :deploy_to, "/mnt/leaapp"
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "10.8.2.172"                          # Your HTTP server, Apache/etc
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
